90bbbfbfabba95958865cb47691557d9b6cc167c
Harsh <harshgajjar145@gmail.com>
2022-10-05 21:04:27 +0530

    fix: fixes

--- components/Work.jsx
+++ components/Work.jsx
@@ -7,16 +7,19 @@ const MotionHeading = motion(Heading);
 
 export default function Work() {
   return (
-    <Container
-      maxW="container.xl"
-      minH="100vh"
-      justifyContent="center"
-    >
-      <MotionHeading fontSize={["6xl", "6xl", "9xl"]} initial={{ opacity: 0 }} whileInView={{ opacity: 1 }}>Work</MotionHeading>
+    <Container maxW="container.xl" minH="100vh" justifyContent="center">
+      <MotionHeading
+        fontSize={["6xl", "6xl", "9xl"]}
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+      >
+        Work
+      </MotionHeading>
       <Stack mt="24">
         {WORK.map((work) => {
           return (
             <WorkCard
+              key={work.id}
               id={work.id}
               name={work.name}
               desc={work.desc}
